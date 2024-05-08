1391049bdbcf3ea35422a5ab758695fc6535211e
Roniher Vilchez <roniher.vilchez@workmate-ext.com>
2024-05-07 20:41:06 -0500

    feat: details from ngFor

--- 02-bases/src/app/dbz/pages/main-page.component.ts
+++ 02-bases/src/app/dbz/pages/main-page.component.ts
@@ -1,7 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './main-page.component.html',
 })
-export class MainPageComponent {}
+export class MainPageComponent {
+  public characters: Character[] = [
+    { name: 'Goku', power: 9500 },
+    { name: 'Krillin', power: 1000 },
+    { name: 'Vegeta', power: 7500 },
+  ];
+}
