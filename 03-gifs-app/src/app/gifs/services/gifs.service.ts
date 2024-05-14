798f22951c567a6f82474903b8e9271d6f1890b5
Roniher Vilchez <roniher.vilchez@workmate-ext.com>
2024-05-13 21:41:03 -0500

    feat: Preserving the data in local storage

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -9,27 +9,42 @@ export class GifsService {
   public gifList: Gifs[] = [];
   private _tagsHistory = new Set<string>();
   private _apiKey = 'vZYnFMJLhqi2o6c0L20Aq5FEZvqk2n0T';
-  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
-  constructor(private http: HttpClient) {}
+  private _serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+  constructor(private http: HttpClient) {
+    this._loadLocalStorage();
+  }
 
   get tagsHistory(): string[] {
     return [...this._tagsHistory];
   }
 
-  private organizeHistory(tag: string): void {
+  private _organizeHistory(tag: string): void {
     tag = tag.trim().toLowerCase();
     this._tagsHistory = new Set([tag, ...this._tagsHistory].slice(0, 10));
+    this._saveLocalStorage();
+  }
+
+  private _saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify([...this._tagsHistory]));
+  }
+
+  private _loadLocalStorage(): void {
+    const history = localStorage.getItem('history');
+    if (!history) return;
+    this._tagsHistory = new Set(JSON.parse(history));
+    const defaultTag = this._tagsHistory.values().next().value ?? '';
+    this.searchTag(defaultTag);
   }
 
   searchTag(tag: string): void {
     if (!tag.trim().length) return;
-    this.organizeHistory(tag);
+    this._organizeHistory(tag);
     const params = new HttpParams()
       .set('api_key', this._apiKey)
       .set('limit', '10')
       .set('q', tag);
     this.http
-      .get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .get<SearchResponse>(`${this._serviceUrl}/search`, { params })
       .subscribe((response) => (this.gifList = response.data));
   }
 }
