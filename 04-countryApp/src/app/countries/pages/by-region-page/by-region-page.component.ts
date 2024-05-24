2347ec5817ecfe79d65f871d7fc3baab0f22a154
Roniher Vilchez <roniher.vilchez@workmate-ext.com>
2024-05-23 20:12:47 -0500

    feat: region options implemented

--- 04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/countries';
 import { CountryService } from '../../services/countries.service';
 
+type Region = 'africa' | 'americas' | 'asia' | 'europe' | 'oceania';
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,9 +11,19 @@ import { CountryService } from '../../services/countries.service';
 export class ByRegionPageComponent {
   public countries: Country[] = [];
   public isLoading: boolean = false;
+  public regions: Region[] = [
+    'africa',
+    'americas',
+    'asia',
+    'europe',
+    'oceania',
+  ];
+  public selectedRegion?: Region;
+
   constructor(private countryService: CountryService) {}
 
-  searchByRegion(term: string): void {
+  searchByRegion(term: Region): void {
+    this.selectedRegion = term;
     this.isLoading = true;
     this.countryService.searchRegion(term).subscribe((countries) => {
       this.countries = countries;
