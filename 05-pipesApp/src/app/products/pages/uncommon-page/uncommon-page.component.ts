be893bc7c0d565729dfdbe6fa578b639be29ff2d
Roniher Vilchez <roniher.vilchez@workmate-ext.com>
2024-05-30 19:16:58 -0500

    feat: valuekey, json and async pipes explained

--- 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
+import { Observable, interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styleUrl: './uncommon-page.component.css'
+  styleUrl: './uncommon-page.component.css',
 })
 export class UncommonPageComponent {
+  public name: string = 'Roniher';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
 
+  changeClient(): void {
+    this.name = 'Melissa';
+    this.gender = 'female';
+  }
+
+  //i18nPlural Pipe
+  public clients: string[] = ['Roniher', 'Melissa', 'Juan', 'Pedro', 'Maria'];
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    other: 'tenemos # clientes esperando.',
+  };
+
+  deleteClient(): void {
+    this.clients.shift();
+  }
+
+  //Keyvalue Pipe
+  public person = {
+    name: 'Roniher',
+    age: 31,
+    address: 'Lima, Perú',
+  };
+
+  //Async Pipe
+  public myObsercableTimer: Observable<number> = interval(2000).pipe(
+    tap((value) => console.log('tap', value))
+  );
+
+  public valuePromise: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data en la promesa.');
+    }, 3500);
+  });
 }
