1c2461b6d532a6b6ef97d7bc1c6a69725046b871
Roniher Vilchez <roniher.vilchez@workmate-ext.com>
2024-06-04 23:47:52 -0500

    feat: SortByPipe implemented

--- 05-pipesApp/src/app/products/pages/order/order.component.ts
+++ 05-pipesApp/src/app/products/pages/order/order.component.ts
@@ -8,6 +8,7 @@ import { Color, Hero } from '../../interfaces/hero.interface';
 })
 export class OrderComponent {
   public isUpperCase: boolean = false;
+  public orderBy: keyof Hero | '' = '';
   public heroes: Hero[] = [
     {
       name: 'Superman',
@@ -33,5 +34,9 @@ export class OrderComponent {
 
   toggleUpperCase(): void {
     this.isUpperCase = !this.isUpperCase;
+  }
+
+  changeOrder(value: keyof Hero): void {
+    this.orderBy = value;
   }
 }
