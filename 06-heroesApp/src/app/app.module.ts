aa2f77a7ac96995ca67393dbcf8e16e8f9252a1f
Roniher Vilchez <roniher.vilchez@workmate-ext.com>
2024-06-07 20:28:32 -0500

    feat: find heroes list

--- 06-heroesApp/src/app/app.module.ts
+++ 06-heroesApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,17 +8,9 @@ import { SharedModule } from './shared/shared.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    AppRoutingModule,
-    BrowserModule,
-    SharedModule
-  ],
-  providers: [
-    provideAnimationsAsync()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [AppRoutingModule, BrowserModule, HttpClientModule, SharedModule],
+  providers: [provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
