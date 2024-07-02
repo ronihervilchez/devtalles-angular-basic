2aa91c4f40aa1edaa74ba4b58c85b24d30bc565b
Roniher Vilchez <roniher.vilchez@workmate-ext.com>
2024-07-01 21:25:17 -0500

    feat: reactive form validators

--- 07-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -14,9 +14,9 @@ export class BasicPageComponent {
 
   //TODO Using FormBuilder
   public myForm: FormGroup = this.fb.group({
-    name: [''],
-    price: [0],
-    inStorage: [0],
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
   });
 
   constructor(private fb: FormBuilder) {}
