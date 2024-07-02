c5d0121133bdb3a141d56daaea17cddb45d712b9
Roniher Vilchez <roniher.vilchez@workmate-ext.com>
2024-07-01 20:18:55 -0500

    feat: reactive basic page form connected

--- 07-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class BasicPageComponent {
+  /* public myForm: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    price: new FormControl(0),
+    inStorage: new FormControl(0),
+  }); */
 
+  //TODO Using FormBuilder
+  public myForm: FormGroup = this.fb.group({
+    name: [''],
+    price: [0],
+    inStorage: [0],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSave(): void {
+    console.log(this.myForm.value);
+  }
 }
