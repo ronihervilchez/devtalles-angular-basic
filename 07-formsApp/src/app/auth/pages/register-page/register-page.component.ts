f65b6443fb5fe50ae1edbfff011c4d06a856d0a1
Roniher Vilchez <roniher.vilchez@workmate-ext.com>
2024-12-15 10:45:08 -0500

    feat: validators centralizados

--- 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as ccustomValidators from '../../../shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: ``
 })
 export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(ccustomValidators.firstNameAndLastnamePattern),
+      ],
+    ],
+    email: [
+      '',
+      [Validators.required, Validators.pattern(ccustomValidators.emailPattern)],
+    ],
+    username: ['', [Validators.required, ccustomValidators.cantBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  });
+  constructor(private readonly fb: FormBuilder) {}
 
+  isValidField(field: string): void {
+    //TODO obtener desde un servicio
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 }
