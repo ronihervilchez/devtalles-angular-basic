96108b98cdde3da9fc238f29f0569c89972fd8e2
Roniher Vilchez <roniher.vilchez@workmate-ext.com>
2024-12-15 10:02:09 -0500

    feat: validaciones personalizadas

--- 07-formsApp/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
-  styles: ``
+  styles: ``,
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
 
+  public person = {
+    gender: 'F',
+    wantNotifications: false,
+  };
+
+  constructor(private readonly fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.myForm.reset(this.person);
+  }
+
+  isValidField(field: string) {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+    const errors = this.myForm.controls[field].errors || {};
+    const erroresMap: Record<string, string> = {
+      required: 'Este campo es requerido.',
+      termsAccepted: 'Debe aceptar los términos y condiciones.',
+    };
+    for (const key of Object.keys(errors)) {
+      if (erroresMap[key]) return erroresMap[key];
+    }
+    return null;
+  }
+
+  //ngSubmit
+  onSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    const { termsAndConditions, ...newPerson } = this.myForm.value;
+    console.log(this.myForm.value);
+    this.person = newPerson;
+    console.log(this.person);
+  }
 }
