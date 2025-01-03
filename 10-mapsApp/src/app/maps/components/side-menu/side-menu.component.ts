afd36d09ae576304fd977971ae317ca0e34a37ed
Roniher Vilchez <roniher.vilchez@workmate-ext.com>
2025-01-03 18:24:30 -0500

    feat: side menu basic structure

--- 10-mapsApp/src/app/maps/components/side-menu/side-menu.component.ts
+++ 10-mapsApp/src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  name: string;
+  route: string;
+}
 @Component({
   selector: 'maps-side-menu',
   templateUrl: './side-menu.component.html',
-  styleUrl: './side-menu.component.css'
+  styleUrl: './side-menu.component.css',
 })
 export class SideMenuComponent {
-
+  public menuItems: MenuItem[] = [
+    { name: 'FullScreen', route: '/maps/fullscreen' },
+    { name: 'ZoomRange', route: '/maps/zoom-range' },
+    { name: 'Markers', route: '/maps/markers' },
+    { name: 'Houses', route: '/maps/properties' },
+  ];
 }
