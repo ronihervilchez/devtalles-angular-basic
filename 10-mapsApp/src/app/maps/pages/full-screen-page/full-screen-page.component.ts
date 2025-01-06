609d89fe3118aff36438aa66118b2ad1d4b729d6
Roniher Vilchez <roniher.vilchez@workmate-ext.com>
2025-01-05 21:40:51 -0500

    feat: Mostrar un mapa en pantalla completa

--- 10-mapsApp/src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
 
 @Component({
   templateUrl: './full-screen-page.component.html',
-  styleUrl: './full-screen-page.component.css'
+  styleUrl: './full-screen-page.component.css',
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit {
+  constructor() {}
 
+  ngAfterViewInit(): void {
+    const map = new maplibregl.Map({
+      container: 'map', // container id
+      style: 'https://demotiles.maplibre.org/style.json', // style URL
+      center: [0, 0], // starting position [lng, lat]
+      zoom: 1, // starting zoom
+    });
+  }
 }
