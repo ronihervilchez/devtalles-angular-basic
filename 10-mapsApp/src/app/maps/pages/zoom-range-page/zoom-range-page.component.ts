075176d76e56dcef99d284a7c909a14563c075b0
Roniher Vilchez <roniher.vilchez@workmate-ext.com>
2025-01-06 22:02:24 -0500

    feat: Obtener las coordenadas centrales del mapa

--- 10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,19 +1,29 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
 
-import { Map } from 'maplibre-gl';
+import { LngLat, Map } from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
   styleUrl: './zoom-range-page.component.css',
 })
-export class ZoomRangePageComponent implements AfterViewInit {
+export class ZoomRangePageComponent implements AfterViewInit, OnDestroy {
   constructor() {}
 
   @ViewChild('map') divMap?: ElementRef;
 
   public zoom: number = 1;
   public map?: Map;
+  public currentLngLat: LngLat = new LngLat(
+    -65.65475865528936,
+    7.653722858034143
+  );
 
   ngAfterViewInit(): void {
     if (!this.divMap) throw new Error('El elemento HTML no fue encontrado');
@@ -21,11 +31,15 @@ export class ZoomRangePageComponent implements AfterViewInit {
     this.map = new Map({
       container: this.divMap.nativeElement, // container id
       style: 'https://demotiles.maplibre.org/style.json', // style URL
-      center: [0, 0], // starting position [lng, lat]
+      center: this.currentLngLat, // starting position [lng, lat]
       zoom: this.zoom, // starting zoom
     });
 
     this.mapListeners();
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove();
   }
 
   mapListeners(): void {
@@ -42,6 +56,11 @@ export class ZoomRangePageComponent implements AfterViewInit {
       if (this.map!.getZoom() > 18) {
         this.map!.zoomTo(18);
       }
+    });
+
+    this.map.on('move', () => {
+      this.currentLngLat = this.map!.getCenter();
+      const { lng, lat } = this.currentLngLat;
     });
   }
 
