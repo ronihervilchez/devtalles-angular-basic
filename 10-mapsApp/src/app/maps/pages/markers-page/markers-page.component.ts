e197ba51a1178e1cf555e2308f079d5288987c4c
Roniher Vilchez <roniher.vilchez@workmate-ext.com>
2025-01-11 15:22:32 -0500

    feat: Moverse hacia un marcador

--- 10-mapsApp/src/app/maps/pages/markers-page/markers-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/markers-page/markers-page.component.ts
@@ -67,4 +67,11 @@ export class MarkersPageComponent {
     this.markers[index].marker.remove();
     this.markers.splice(index, 1);
   }
+
+  flyTo(marker: Marker): void {
+    this.map?.flyTo({
+      zoom: 13,
+      center: marker.getLngLat(),
+    });
+  }
 }
