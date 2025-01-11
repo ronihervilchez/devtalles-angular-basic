62aa321e6ef457c29cff8d32d9959782eed99c03
Roniher Vilchez <roniher.vilchez@workmate-ext.com>
2025-01-11 16:05:08 -0500

    feat: Componente MiniMapa

--- 10-mapsApp/src/app/maps/components/mini-map/mini-map.component.ts
+++ 10-mapsApp/src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
+import { LngLat, Map, Marker } from 'maplibre-gl';
 
 @Component({
-  selector: 'maps-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
-  styleUrl: './mini-map.component.css'
+  styleUrl: './mini-map.component.css',
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit {
+  @ViewChild('map') divMap?: ElementRef;
+  @Input() lngLat: [number, number] = [0, 0];
 
+  public map?: Map;
+  public zoom: number = 15;
+
+  ngAfterViewInit(): void {
+    if (!this.divMap?.nativeElement)
+      throw new Error('El elemento HTML no fue encontrado');
+    if (!this.lngLat) throw new Error('Falta el valor de lngLat');
+
+    // mapa
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container id
+      style: 'https://demotiles.maplibre.org/style.json', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: this.zoom, // starting zoom
+      interactive: false,
+    });
+
+    // marker
+
+    const color = '#XXXXXX'.replace(/X/g, () =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+    const lngLat = this.map.getCenter();
+    this.addMarker(lngLat, color);
+  }
+
+  addMarker(lngLat: LngLat, color: string): void {
+    if (!this.map) return;
+
+    new Marker({
+      color,
+      draggable: false,
+    })
+      .setLngLat(lngLat)
+      .addTo(this.map);
+  }
 }
