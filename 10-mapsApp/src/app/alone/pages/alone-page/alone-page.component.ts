afa10372b31de460b156a83da273524940af1a81
Roniher Vilchez <roniher.vilchez@workmate-ext.com>
2025-01-31 08:32:34 -0500

    feat: standalone como componente reutilizable

---
+++ 10-mapsApp/src/app/alone/pages/alone-page/alone-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
+
+@Component({
+  standalone: true,
+  imports: [CounterAloneComponent],
+  templateUrl: './alone-page.component.html',
+  styleUrl: './alone-page.component.css',
+})
+export class AlonePageComponent {
+  constructor() {}
+}
