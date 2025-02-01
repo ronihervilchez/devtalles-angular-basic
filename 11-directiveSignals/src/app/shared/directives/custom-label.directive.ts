6ac253e4b9b93bd7dff01186f40ed9561991b38c
Roniher Vilchez <roniher.vilchez@workmate-ext.com>
2025-02-01 09:02:23 -0500

    feat: directiva de errores

--- 11-directiveSignals/src/app/shared/directives/custom-label.directive.ts
+++ 11-directiveSignals/src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[custonLabel]',
@@ -6,10 +7,16 @@ import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 export class CustomLabelDirective implements OnInit {
   private readonly htmlElement?: ElementRef<HTMLElement>;
   private _color: string = 'red';
+  private _errors?: ValidationErrors | null;
 
   @Input() set color(value: string) {
     this._color = value;
     this.setStyle();
+  }
+
+  @Input() set errors(value: ValidationErrors | null | undefined) {
+    this._errors = value;
+    this.setErrorMessage();
   }
 
   constructor(private readonly el: ElementRef<HTMLElement>) {
@@ -23,5 +30,31 @@ export class CustomLabelDirective implements OnInit {
   setStyle(): void {
     if (!this.htmlElement) return;
     this.htmlElement.nativeElement.style.color = this._color;
+  }
+
+  setErrorMessage(): void {
+    if (!this.htmlElement) return;
+
+    if (!this._errors) {
+      this.htmlElement.nativeElement.innerHTML = 'No hay errores';
+      return;
+    }
+    const errors = Object.keys(this._errors);
+    if (errors.includes('required')) {
+      this.htmlElement.nativeElement.innerHTML = 'Este campo es requerido';
+      return;
+    }
+
+    if (errors.includes('minlength')) {
+      const min = this._errors['minlength'].requiredLength;
+      const current = this._errors['minlength'].actualLength;
+      this.htmlElement.nativeElement.innerHTML = `Mínimo ${current}/${min} caracteres`;
+      return;
+    }
+
+    if (errors.includes('email')) {
+      this.htmlElement.nativeElement.innerHTML = 'No tiene formato de correo';
+      return;
+    }
   }
 }
