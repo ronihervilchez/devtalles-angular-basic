51b28b947f5ce7d60e1a89c2ad2f51be3e892eb5
Roniher Vilchez <roniher.vilchez@workmate-ext.com>
2025-02-01 07:57:25 -0500

    feat: Formulario reactivo tradicional

--- 11-directiveSignals/src/app/products/pages/product-page/product-page.component.ts
+++ 11-directiveSignals/src/app/products/pages/product-page/product-page.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './product-page.component.html',
-  styleUrl: './product-page.component.css'
+  styleUrl: './product-page.component.css',
 })
 export class ProductPageComponent {
+  // constructor(private readonly fb: FormBuilder) {}
+  private readonly fb = inject(FormBuilder);
+  public color: string = 'green';
+  public myForm: FormGroup = this.fb.group({
+    name: [
+      '',
+      [Validators.required, Validators.minLength(6), Validators.email],
+    ],
+  });
 
+  changeColor(): void {
+    this.color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+  }
 }
--- 11-directiveSignals/src/app/products/products.module.ts
+++ 11-directiveSignals/src/app/products/products.module.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { ProductsRoutingModule } from './products-routing.module';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { ProductsRoutingModule } from './products-routing.module';
 
 @NgModule({
-  declarations: [
-    ProductPageComponent
-  ],
-  imports: [CommonModule, ProductsRoutingModule],
+  declarations: [ProductPageComponent],
+  imports: [CommonModule, ProductsRoutingModule, ReactiveFormsModule],
 })
 export class ProductsModule {}
